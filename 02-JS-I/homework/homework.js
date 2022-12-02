288a0f98ae783f609b852e9106ae377daf8e7b01
RivamaDave <rivamadave@gmail.com>
2022-12-02 01:01:41 -0600

    new commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Holaa';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 37;
+const nuevoNum = 8
 
 // Crea una variable booleana:
 const nuevoBool = true;
@@ -224,7 +224,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro * 1.2 dolar
 }
 
 
